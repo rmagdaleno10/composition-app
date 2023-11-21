c0dae8589dbe48a40f3f176df0140c922bc0413e
ramon-it <rmagdaleno5v@gmail.com>
2023-11-20 21:21:43 -0600

    Axios and Get Http Request to API

--- src/pokemons/pages/PokemonList.vue
+++ src/pokemons/pages/PokemonList.vue
@@ -1,3 +1,12 @@
+
+<script setup lang="ts">
+
+import pokemonApi from '../api/pokemonApi';
+pokemonApi.get('/pokemon?limit=10')
+    .then( response => console.log(response.data.results) );
+
+</script>
+
 <template>
     <div>
         <h1>Pokemon List</h1>
@@ -5,6 +14,3 @@
     </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
